2c8a666832b20cb79c251b6e2b2f560732478b9b
Pavel <voroshilow_pavel@mail.ru>
2025-04-13 09:57:39 +0300

    pages

--- public/index.php
+++ public/index.php
@@ -8,10 +8,25 @@ use Slim\Factory\AppFactory;
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
-    return $response;
-    // Благодаря пакету slim/http этот же код можно записать короче
-    // return $response->write('Welcome to Slim!');
+$links = [
+    ['city'=>'Moscow', 'total'=>20000],
+    ['city'=>'Paris', 'total'=>19843],
+    ['city'=>'London', 'total'=>17890],
+];
+
+
+$app->get('/users', function ($request, $response) {
+    return $response->write('GET /users');
 });
+
+$app->post('/users', function ($request, $response) {
+    return $response->write('POST /users');
+});
+
+$app->get('/pages', function ($request, $response) use ($links) {
+    return $response->write(json_encode($links));
+});
+
+
+
 $app->run();
