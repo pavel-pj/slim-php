edad8c9ce6cf3c3fd3d329827fa038c3d4a43ca6
Pavel <voroshilow_pavel@mail.ru>
2025-04-13 15:24:55 +0300

    paginator

--- public/index.php
+++ public/index.php
@@ -8,6 +8,8 @@ use Slim\Factory\AppFactory;
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
+ $companies = App\Generator::generate(100);
+
 $links = [
     ['city'=>'Moscow', 'total'=>20000],
     ['city'=>'Paris', 'total'=>19843],
@@ -15,17 +17,15 @@ $links = [
 ];
 
 
-$app->get('/users', function ($request, $response) {
-    return $response->write('GET /users');
+$app->get('/companies', function ($request, $response)  use ($companies)  {
+    $page = $request->getQueryParam('page', 1);
+    $per = $request->getQueryParam('per', 5);
+    $info = array_splice($companies, $page * $per, $per);
+    $json = json_encode($info);
+
+    return $response->write($json);
 });
 
-$app->post('/users', function ($request, $response) {
-    return $response->write('POST /users');
-});
-
-$app->get('/pages', function ($request, $response) use ($links) {
-    return $response->write(json_encode($links));
-});
 
 
 
